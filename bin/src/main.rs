fe9f4c135aa8feafa7c035d00d477abb0a50dcec
Lee Lazarecky <leelazarecky@gmail.com>
2022-09-25 15:39:02 -0400

    Initial commit

---
+++ bin/src/main.rs
@@ -0,0 +1,8 @@
+use chip8;
+
+fn main() {
+
+    let c8 = chip8::new();
+
+    println!("Hello, world!");
+}
---
+++ chip8/src/lib.rs
@@ -0,0 +1,6 @@
+extern crate core;
+
+mod chip8;
+mod fonts;
+
+pub use crate::chip8::*;
